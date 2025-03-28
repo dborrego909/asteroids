119f748d5af2b8f43be345fcaf8c993115e1f841
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2025-03-27 20:59:04 -0700

    Player able to turn left and right

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player =  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while (True):
@@ -13,7 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.display.flip()
+        
 
 
 
